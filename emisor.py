a711a3ccc4994e626b3b1e979bc7e7736148471a
fdquinones1986 <140764717+fdquinones1986@users.noreply.github.com>
2024-10-22 23:48:17 +0000

    Ajustes iniciales

---
+++ emisor.py
@@ -0,0 +1,39 @@
+import paho.mqtt.client as mqtt
+import time
+
+
+BROKER = 'broker.emqx.io'
+PORT = 1883
+TOPIC = "python-mqtt/tcp"
+# generate client ID with pub prefix randomly
+CLIENT_ID = f'python-mqtt-tcp-pub-sub-{random.randint(0, 1000)}'
+USERNAME = 'emqx'
+PASSWORD = 'public'
+
+def on_publish(client, userdata, mid):
+    print("message published")
+
+def connect_mqtt():
+    client = mqtt_client.Client(mqtt_client.CallbackAPIVersion.VERSION1, CLIENT_ID)
+    client.username_pw_set(USERNAME, PASSWORD)
+    client.on_connect = on_connect
+    client.connect(BROKER, PORT, keepalive=120)
+    return client
+
+
+def run():
+    logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s',
+                        level=logging.DEBUG)
+    client = connect_mqtt()
+
+    while True:
+        # Mensaje a publicar
+        msg = "Hola desde Python!"
+
+        # Publicar el mensaje
+        ret = client.publish(TOPIC, msg)
+
+        time.sleep(1)
+
+if __name__ == '__main__':
+    run()
